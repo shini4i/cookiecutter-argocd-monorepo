f5733f6bade69d9f31fab27c4d195ef4ed1400be
Vadim Gedz <vadims@linux-tech.io>
2024-08-25 23:51:24 +0300

    chore: bump project dependencies (#2)

--- {{cookiecutter.project_name}}/{{cookiecutter.cluster_name}}/terraform/deploy/main.tf
+++ {{cookiecutter.project_name}}/{{cookiecutter.cluster_name}}/terraform/deploy/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.12.1"
+      version = "2.15.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
@@ -12,7 +12,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.24.0"
+      version = "2.32.0"
     }
   }
 }
